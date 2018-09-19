d9a0efc56821dc90ea4ca3967765e8d8528f8c79
Bimal Lamichhane <linkbimal123@gmail.com>
2018-09-19 11:34:02 +0545

    # working with the logo

---
+++ src/components/Header/Accounts.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="#accounts">
+        <div id="logo">
+            <img src="../../assets/logo1.png" alt="">
+        </div>
+        <div id="account-options">
+
+        </div>
+    </div>
+</template>
