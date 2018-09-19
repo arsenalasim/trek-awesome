064e5b8d080da5e67dcf9b8430d6119f6a1b2c39
Bimal Lamichhane <linkbimal123@gmail.com>
2018-09-19 11:49:48 +0545

    #checking with local host

--- src/components/Header/Accounts.vue
+++ src/components/Header/Accounts.vue
@@ -1,10 +1,25 @@
 <template>
     <div id="#accounts">
+        <p>gdfjhfcsdhfkshfkusdjhdkusagc sdfgh sdkg </p>
         <div id="logo">
-            <img src="../../assets/logo1.png" alt="">
+            <img :src="image" alt="compant logo">
+            
         </div>
         <div id="account-options">
 
         </div>
     </div>
 </template>
+
+<script>
+import logo1 from '../../assets/logo1.png'
+export default {
+    data:function(){
+        return{
+            image:logo1
+        }
+    }
+}
+</script>
+
+
